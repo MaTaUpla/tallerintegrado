d5225c94c5ae6273a6219b9ee949810a5dc7697d
MaTaUpla <89056752+MaTaUpla@users.noreply.github.com>
2021-08-17 17:07:30 -0400

    Update run.py
    se actualiza codigo para hacer el grafico y llamar funciones a las rutas.

--- Webapp/run.py
+++ Webapp/run.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests, json, sys
 import api_twitch
+import io 
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+
+  fig,ax = plt.subplots(figsize=(10,8))
+  ax = sns.set_style(style="dark")
+
   live = {}
   live['data'] = []
   live_stream = []
@@ -41,6 +50,13 @@ def home():
     'visitas4' : live_stream[14],
     'url4' : live_stream[15]
   }
+  x = [live_stream[0],live_stream[4],live_stream[8],live_stream[12]]
+  y = [live_stream[2],live_stream[6],live_stream[10],live_stream[14]]
+  sns.barplot(x,y)
+  canvas=FigureCanvas(fig)
+  img=io.BytesIO()
+  fig.savefig('static/img/grafico.png')
+
   return render_template("home.html",data=data)
 
 @app.route('/Streamer', methods=["POST","GET"])
